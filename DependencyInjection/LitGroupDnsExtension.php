5424965a4df38e0f37367a4db1ab71cf5f1e886d
Roman Shamritskiy <roman@litgroup.ru>
2014-06-30 16:48:21 +0400

    Provides litgroup_dns.resolver service.

--- DependencyInjection/LitGroupDnsExtension.php
+++ DependencyInjection/LitGroupDnsExtension.php
@@ -13,6 +13,8 @@ namespace LitGroup\Bundle\DnsBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Config\FileLocator;
 
 /**
  * LitGroupDnsExtension
@@ -33,7 +35,25 @@ class LitGroupDnsExtension extends Extension
      */
     public function load(array $config, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $config);
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 
+        $this->loadResolverFactory($loader);
+        $this->loadResolver($config, $container, $loader);
     }
 
-} 
+    private function loadResolverFactory(Loader\XmlFileLoader $loader)
+    {
+        $loader->load('resolver_factory.xml');
+    }
+
+    private function loadResolver(array $config, ContainerBuilder $container, Loader\XmlFileLoader $loader)
+    {
+        $loader->load('resolver.xml');
+        $definition = $container->getDefinition('litgroup_dns.resolver');
+        $definition->replaceArgument(0, $config['nameserver']);
+        $definition->setFactoryMethod($config['cache'] ? 'createCached' : 'create');
+    }
+
+}
--- Tests/DependencyInjection/LitGroupDnsExtensionTest.php
+++ Tests/DependencyInjection/LitGroupDnsExtensionTest.php
@@ -11,21 +11,73 @@
 namespace LitGroup\Bundle\DnsBundle\Tests\DependencyInjection;
 
 
+use LitGroup\Bundle\DnsBundle\DependencyInjection\LitGroupDnsExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
 class LitGroupDnsExtensionTest extends \PHPUnit_Framework_TestCase
 {
+    /** @test */
     public function testFactory()
     {
-        $this->markTestIncomplete();
+        $container = new ContainerBuilder();
+        $extension = new LitGroupDnsExtension();
+        $extension->load([$this->getDefaultConfig()], $container);
+
+        $this->assertTrue($container->hasParameter('litgroup_dns.resolver_factory.class'));
+        $this->assertSame(
+            'React\Dns\Resolver\Factory',
+            $container->getParameter('litgroup_dns.resolver_factory.class')
+        );
+
+        $this->assertTrue($container->hasDefinition('litgroup_dns.resolver_factory'));
+
+        $definition = $container->getDefinition('litgroup_dns.resolver_factory');
+        $this->assertSame('%litgroup_dns.resolver_factory.class%', $definition->getClass());
+        $this->assertTrue($definition->isPublic());
     }
 
-    public function testResolver()
+    public function getResolverTests()
     {
-        $this->markTestIncomplete();
+        return [
+            [ ['cache' => false], 'create' ],
+            [ ['cache' => true], 'createCached' ],
+        ];
     }
 
-    public function testCachedResolver()
+    /** @dataProvider getResolverTests */
+    public function testResolver($config, $factoryMethod)
     {
-        $this->markTestIncomplete();
+        $container = new ContainerBuilder();
+        $extension = new LitGroupDnsExtension();
+        $config = array_merge($this->getDefaultConfig(), $config);
+        $extension->load([$config], $container);
+
+        $this->assertTrue($container->hasDefinition('litgroup_dns.resolver'));
+        $definition = $container->getDefinition('litgroup_dns.resolver');
+        $this->assertTrue($definition->isPublic());
+        $this->assertSame('React\Dns\Resolver\Resolver', $definition->getClass());
+        $this->assertSame('litgroup_dns.resolver_factory', $definition->getFactoryService());
+        $this->assertSame($factoryMethod, $definition->getFactoryMethod());
+        $arguments = $definition->getArguments();
+        $this->assertCount(2, $arguments);
+        $this->assertSame($config['nameserver'], $arguments[0]);
+        $this->assertReference('litgroup_event_loop', $arguments[1]);
+
+
+    }
+
+    private function assertReference($expectedId, $reference)
+    {
+        $this->assertInstanceOf('Symfony\Component\DependencyInjection\Reference', $reference);
+        $this->assertSame($expectedId, $reference->__toString());
+    }
+
+    /**
+     * @return array
+     */
+    private function getDefaultConfig()
+    {
+        return ['nameserver' => '127.0.0.1'];
     }
 
 }
